44d2abe0797eda71555feff93a1dde6304c9474a
Nicholas <info@squallstar.it>
2011-08-25 20:07:14 +0200

    Various localizations

--- application/views/admin/content/type_delete.php
+++ application/views/admin/content/type_delete.php
@@ -6,10 +6,10 @@ $this->load->helper('form');
 		<div class="bheadl"></div>
 		<div class="bheadr"></div>
 
-		<h2>Eliminazione tipo di contenuto: <?php echo $tipo['description']; ?></h2>
+		<h2><?php echo _('Type delete'); ?>: <?php echo $tipo['description']; ?></h2>
 
 		<ul>
-			<li><a href="<?php echo admin_url($_section.'/')?>">Annulla</a></li>
+			<li><a href="<?php echo admin_url($_section.'/')?>"><?php echo _('Cancel'); ?></a></li>
 		</ul>
 
 
@@ -27,15 +27,15 @@ $this->load->helper('form');
 	</ul>
 
 <?php if ($this->config->item('delete_dead_recods')) { ?>
-	<div class="message warning"><p>ATTENZIONE: Tutti i contenuti associati al tipo di contenuto verranno eliminati.<br />Puoi disattivare questa funzionalit&agrave; modificando il valore della variabile <u>DEAD RECORDS</u> nel file di configurazione di Milk.</p></div>
+	<div class="message warning"><p><?php echo _('WARNING'); ?>: <?php echo _('All contents associated to this type will be deleted.'); ?><br /><?php echo _('You can disable this function through the variabile "DEAD RECORDS" in the config file.'); ?></p></div>
 
 <?php }
 
 echo form_open();
 echo form_hidden('id_type', $tipo['id']);
 
-echo br(1) . form_submit('delete', 'Conferma eliminaz.', 'class="submit long"');
-echo form_submit('cancel', 'Annulla', 'class="submit mid"') . br(2);
+echo br(1) . form_submit('delete', _('Confirm deletion'), 'class="submit long"');
+echo form_submit('cancel', _('Cancel'), 'class="submit mid"') . br(2);
 
 echo form_close();
 
