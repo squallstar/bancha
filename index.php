1cb58045dca1dc0c922faf1671a57b754f1fdc89
Nicholas Valbusa <info@squallstar.it>
2011-08-26 12:27:45 +0200

    intestazione

--- index.php
+++ index.php
@@ -1,4 +1,16 @@
 <?php
+/**
+ * Index Collector
+ *
+ * File collettore delle richieste
+ *
+ * @package		Milk
+ * @author		Nicholas Valbusa - info@squallstar.it - @squallstar
+ * @copyright	Copyright (c) 2011, Squallstar
+ * @license		GNU/GPL (General Public License)
+ * @link		http://squallstar.it
+ *
+ */
 
 /*
  *---------------------------------------------------------------
