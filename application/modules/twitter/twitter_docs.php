152be8c6bca194bdbefe1a46005bc979288993e3
Nicholas Valbusa <info@squallstar.it>
2011-08-26 17:22:16 +0200

    twitter doc intro

--- application/modules/twitter/twitter_docs.php
+++ application/modules/twitter/twitter_docs.php
@@ -1,1 +1,14 @@
-Hello world
+<div class="sidebar">
+	<ul class="sidemenu">
+		<li><a href="#sb-intro">Introduzione</a></li>
+	</ul>
+<p>
+	Author: Nicholas Valbusa<br />
+	Package: Milk
+</p>
+</div>
+
+<div class="sidebar_content" id="sb-intro">
+	<h3>Introduzione</h3>
+	<p>Lorem ipsum dolor sit amet...</p>
+</div>
