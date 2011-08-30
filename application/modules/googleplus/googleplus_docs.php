998bc260fa8a912568918ae15db8db7860611606
Nicholas Valbusa <info@squallstar.it>
2011-08-30 14:28:38 +0200

    googleplus module documentation

---
+++ application/modules/googleplus/googleplus_docs.php
@@ -0,0 +1,17 @@
+<div class="sidebar">
+	<ul class="sidemenu">
+		<li><a href="#sb-intro">Utilizzo</a></li>
+	</ul>
+<p>
+	Author: Nicholas Valbusa<br />
+	Package: Milk
+</p>
+</div>
+
+<div class="sidebar_content" id="sb-intro">
+	<h3>Utilizzo del modulo Google Plus</h3>
+	<p>Questo modulo, stampa a video il pulsante <strong>+1</strong> di Google Plus.</p>
+	<p>L'utilizzo è molto semplice, basta inserire questo codice in una qualsiasi view:</p>
+<code>&lt;?php $this->load->module('googleplus')->render(); ?&gt;</code>
+
+</div>
