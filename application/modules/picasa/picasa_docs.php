87aa3f5fdec26cb78204e94ba00e7061d1890b3e
alexmaroldi <alexmaroldi@gmail.com>
2011-09-14 19:38:35 +0200

    Picasa Modules

---
+++ application/modules/picasa/picasa_docs.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Sharer Module Documentation
+ *
+ * @package		Bancha
+ * @author		Nicholas Valbusa - info@squallstar.it - @squallstar
+ * @copyright	Copyright (c) 2011, Squallstar
+ * @license		GNU/GPL (General Public License)
+ * @link		http://squallstar.it
+ *
+ */
+
+?><div class="sidebar">
+	<ul class="sidemenu">
+		<li><a href="#sb-intro">Using the module</a></li>
+	</ul>
+<p>
+	Author: Alessandro Maroldi<br />
+	Package: Bancha
+</p>
+</div>
+
+<div class="sidebar_content" id="sb-intro">
+	<h3>Using the picasa module</h3>
+	<p>This module is useful to gets the sharing URL for Facebook/Twitter or to renders a sharing button.</p>
+</div>
---
+++ application/modules/picasa/picasa_module.php
@@ -0,0 +1,176 @@
+<?php
+/**
+ * Picasa Module
+ *
+ * This module let you connect to Picasa account and get the gallery.
+ *
+ * Usage:
+ * $picasa = $this->load->module('picasa');
+ *
+ * //Renders the sharing button
+ * echo $picasa->type('facebook')
+ * 		 	   ->url('http://example.org')
+ * 		  	   ->title('Share on Facebook')
+ * 		  	   ->render();
+ *
+ *
+ * @package		Bancha
+ * @author		Alessandro Maroldi - alexmaroldi@gmail.com - @alexmaroldi
+ * @copyright	Copyright (c) 2011, Squallstar
+ * @license		GNU/GPL (General Public License)
+ *
+ */
+
+Class Picasa_Module extends Bancha_Module
+{
+
+  /**
+   * @var string URL for Picasa API
+   */	
+   private $_url = 'http://picasaweb.google.com/data/feed/api/';
+   
+  /**
+   * @var string Username of Picasa account
+   */
+  private $_username = 'kikkovolley';
+  
+  /**
+   * @var string Container of for Picasa gallery
+   */
+  private $_container = 'gallery';
+  
+  public function __construct()
+  {
+    parent::__construct();
+  }
+  
+  
+  public function getAlbums($maxAlbums = FALSE)
+  {
+  		$_url;
+		//var url = 'http://picasaweb.google.com/data/feed/api/user/' + pwi_username + '?category=album' + (pwi_maxalbums > 0 ? '&max-results=' + pwi_maxalbums: "") + '&access=public&alt=json';
+  }
+
+  /**
+   * Sets the sharing URL (default is the current url)
+   * @param string $link
+   */
+  public function url($link = '')
+  {
+    if ($link != '')
+    {
+      $this->_url = $link;
+    }
+    return $this;
+  }
+
+  /**
+   * Sets the twitter sharing text
+   * @param string $message
+   */
+  public function text($message = '')
+  {
+    if ($message != '')
+    {
+      $this->_text = $message;
+    }
+    return $this;
+  }
+
+  /**
+   * Sets the twitter sharing text
+   * Same function of $this->text();
+   * @param string $message
+   */
+  public function message($message = '')
+  {
+    return $this->text($message);
+  }
+
+  /**
+   * Sets the Twitter "via" param (also known as @username)
+   * @param string $username
+   */
+  public function via($username = '')
+  {
+    if ($username != '')
+    {
+      $this->_via = str_replace('@', '', $username);
+    }
+    return $this;
+  }
+
+  /**
+   * Sets the Twitter hashtags
+   * @param string $tags
+   */
+  public function hashtags($tags = '')
+  {
+    if ($tags != '')
+    {
+      $this->_tags = $tags;
+    }
+    return $this;
+  }
+
+  /**
+   * Sets the social network
+   * @param string $social
+   */
+  public function type($social = '')
+  {
+    $social = strtolower($social);
+    if ($social != '' && in_array($social, $this->_types))
+    {
+      $this->_type = $social;
+    }
+    return $this;
+  }
+
+  /**
+   * Renders the sharing button
+   * @see application/libraries/bancha/Bancha_Module::render()
+   * @return xhtml
+   */
+  public function render()
+  {
+    if ($this->_type != '')
+    {
+      $this->get_link();
+      $this->view->set('_sharer', array(
+        'type'	=> $this->_type,
+        'url'	=> $this->_url,
+        'link'	=> $this->_link,
+        'title'	=> $this->_title != '' ? $this->_title : $this->view->title
+      ));
+      return parent::render();
+    }
+  }
+
+  /**
+   * Gets the sharing link
+   * @return string
+   */
+  public function get_link()
+  {
+    $this->_url = $this->_url != '' ? $this->_url : current_url();
+
+    switch ($this->_type)
+    {
+      case 'facebook':
+        $this->_link = '"http://www.facebook.com/sharer/sharer.php?u=' . urlencode($this->_url);
+        break;
+
+      case 'twitter':
+        $this->_link = 'http://twitter.com/intent/tweet?via=' . urlencode($this->_via) . '&hashtags=' . urlencode($this->_tags)
+               . '&text=' . urlencode($this->_text) . '%20' . urlencode($this->_url);
+        break;
+
+      default:
+        $this->_link = $this->_url;
+    }
+    return $this->_link;
+  }
+
+
+}
