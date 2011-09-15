813f7f1d43b689778c19fd6f5411e3050a4b7a02
Nicholas Valbusa <info@squallstar.it>
2011-09-15 17:07:25 +0200

    tabulazione sbagliata

--- application/modules/sharer/sharer_module.php
+++ application/modules/sharer/sharer_module.php
@@ -30,148 +30,146 @@
 
 Class Sharer_Module extends Bancha_Module
 {
-  /**
-   * @var string The social network
-   */
-  private $_type = '';
+    /**
+    * @var string The social network
+    */
+    private $_type = '';
 
-  /**
-   * @var array Defined socials
-   */
-  private $_types = array('facebook', 'twitter', 'tumblr');
+    /**
+    * @var array Defined socials
+    */
+    private $_types = array('facebook', 'twitter');
 
-  private $_url	= '';
-  private $_link	= '';
-  private $_title	= '';
-  private $_via	= '';
-  private $_tags	= '';
-  private $_text	= '';
+    private $_url	= '';
+    private $_link	= '';
+    private $_title	= '';
+    private $_via	= '';
+    private $_tags	= '';
+    private $_text	= '';
 
-  public function __construct()
-  {
-    parent::__construct();
-  }
-
-  /**
-   * Sets the sharing URL (default is the current url)
-   * @param string $link
-   */
-  public function url($link = '')
-  {
-    if ($link != '')
+    public function __construct()
     {
-      $this->_url = $link;
+        parent::__construct();
     }
-    return $this;
-  }
 
-  /**
-   * Sets the twitter sharing text
-   * @param string $message
-   */
-  public function text($message = '')
-  {
-    if ($message != '')
+    /**
+    * Sets the sharing URL (default is the current url)
+    * @param string $link
+    */
+    public function url($link = '')
     {
-      $this->_text = $message;
+        if ($link != '')
+        {
+            $this->_url = $link;
+        }
+        return $this;
     }
-    return $this;
-  }
 
-  /**
-   * Sets the twitter sharing text
-   * Same function of $this->text();
-   * @param string $message
-   */
-  public function message($message = '')
-  {
-    return $this->text($message);
-  }
-
-  /**
-   * Sets the Twitter "via" param (also known as @username)
-   * @param string $username
-   */
-  public function via($username = '')
-  {
-    if ($username != '')
+    /**
+    * Sets the twitter sharing text
+    * @param string $message
+    */
+    public function text($message = '')
     {
-      $this->_via = str_replace('@', '', $username);
+        if ($message != '')
+        {
+            $this->_text = $message;
+        }
+        return $this;
     }
-    return $this;
-  }
 
-  /**
-   * Sets the Twitter hashtags
-   * @param string $tags
-   */
-  public function hashtags($tags = '')
-  {
-    if ($tags != '')
+    /**
+    * Sets the twitter sharing text
+    * Same function of $this->text();
+    * @param string $message
+    */
+    public function message($message = '')
     {
-      $this->_tags = $tags;
+        return $this->text($message);
     }
-    return $this;
-  }
 
-  /**
-   * Sets the social network
-   * @param string $social
-   */
-  public function type($social = '')
-  {
-    $social = strtolower($social);
-    if ($social != '' && in_array($social, $this->_types))
+    /**
+    * Sets the Twitter "via" param (also known as @username)
+    * @param string $username
+    */
+    public function via($username = '')
     {
-      $this->_type = $social;
+        if ($username != '')
+        {
+            $this->_via = str_replace('@', '', $username);
+        }
+        return $this;
     }
-    return $this;
-  }
 
-  /**
-   * Renders the sharing button
-   * @see application/libraries/bancha/Bancha_Module::render()
-   * @return xhtml
-   */
-  public function render()
-  {
-    if ($this->_type != '')
+    /**
+    * Sets the Twitter hashtags
+    * @param string $tags
+    */
+    public function hashtags($tags = '')
     {
-      $this->get_link();
-      $this->view->set('_sharer', array(
-        'type'	=> $this->_type,
-        'url'	=> $this->_url,
-        'link'	=> $this->_link,
-        'title'	=> $this->_title != '' ? $this->_title : $this->view->title
-      ));
-      return parent::render();
+        if ($tags != '')
+        {
+            $this->_tags = $tags;
+        }
+        return $this;
     }
-  }
 
-  /**
-   * Gets the sharing link
-   * @return string
-   */
-  public function get_link()
-  {
-    $this->_url = $this->_url != '' ? $this->_url : current_url();
-
-    switch ($this->_type)
+    /**
+    * Sets the social network
+    * @param string $social
+    */
+    public function type($social = '')
     {
-      case 'facebook':
-        $this->_link = '"http://www.facebook.com/sharer/sharer.php?u=' . urlencode($this->_url);
-        break;
-
-      case 'twitter':
-        $this->_link = 'http://twitter.com/intent/tweet?via=' . urlencode($this->_via) . '&hashtags=' . urlencode($this->_tags)
-               . '&text=' . urlencode($this->_text) . '%20' . urlencode($this->_url);
-        break;
-
-      default:
-        $this->_link = $this->_url;
+        $social = strtolower($social);
+        if ($social != '' && in_array($social, $this->_types))
+        {
+            $this->_type = $social;
+        }
+        return $this;
     }
-    return $this->_link;
-  }
 
+    /**
+    * Renders the sharing button
+    * @see application/libraries/bancha/Bancha_Module::render()
+    * @return xhtml
+    */
+    public function render()
+    {
+        if ($this->_type != '')
+        {
+            $this->get_link();
+            $this->view->set('_sharer', array(
+                'type'	=> $this->_type,
+                'url'	=> $this->_url,
+                'link'	=> $this->_link,
+                'title'	=> $this->_title != '' ? $this->_title : $this->view->title
+            ));
+            return parent::render();
+        }
+    }
 
+    /**
+    * Gets the sharing link
+    * @return string
+    */
+    public function get_link()
+    {
+        $this->_url = $this->_url != '' ? $this->_url : current_url();
+
+        switch ($this->_type)
+        {
+            case 'facebook':
+                $this->_link = '"http://www.facebook.com/sharer/sharer.php?u=' . urlencode($this->_url);
+                break;
+
+            case 'twitter':
+                $this->_link = 'http://twitter.com/intent/tweet?via=' . urlencode($this->_via) . '&hashtags=' . urlencode($this->_tags)
+                . '&text=' . urlencode($this->_text) . '%20' . urlencode($this->_url);
+                break;
+
+            default:
+                $this->_link = '';
+        }
+        return $this->_link;
+    }
 }
