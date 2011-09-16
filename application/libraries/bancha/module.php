3bb00811245def5930dab3ad1e247e972354761a
Nicholas Valbusa <info@squallstar.it>
2011-09-16 10:15:33 +0200

    added an option to load a class inside the same module

--- application/libraries/bancha/module.php
+++ application/libraries/bancha/module.php
@@ -21,6 +21,9 @@ abstract class Bancha_Module
 	 */
 	public $view;
 
+	public $module_filespath;
+	public $module_name;
+
 	public function __construct()
 	{
 		$CI = & get_instance();
@@ -37,5 +40,12 @@ abstract class Bancha_Module
 		$module_name = strtolower(str_replace('_Module', '', get_class($this)));
 		$CI->load->add_view_path($CI->config->item('modules_folder'));
 		return $CI->load->view($module_name.DIRECTORY_SEPARATOR.$module_name.'_'.$view, $CI->view->get_data(), TRUE);
+	}
+
+	public function load($module_file)
+	{
+		require_once $this->module_filespath . strtolower($module_file) . '.php';
+		$class_name = $this->module_name . '_' . ucfirst($module_file);
+		return new $class_name();
 	}
 }
