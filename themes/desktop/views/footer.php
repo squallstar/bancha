6fe819d96a92fb8529799e2c04d5c72b1731f164
unknown <Matt@.(none)>
2011-09-18 17:13:27 +0200

    HTML5 theme

--- themes/desktop/views/footer.php
+++ themes/desktop/views/footer.php
@@ -1,19 +1,5 @@
 <?php if(!defined('BASEPATH')) exit('No direct script access allowed'); ?>
-<div class="col">
-	<h3><a href="#">Ut enim risus rhoncus</a></h3>
-	<p>Quisque consectetur odio ut sem semper commodo. Maecenas iaculis leo a ligula euismod condimentum. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
-	<p>&not; <a href="#">read more</a></p>
-</div>
-<div class="col">
-	<h3><a href="#">Maecenas iaculis leo</a></h3>
-	<p>Quisque consectetur odio ut sem semper commodo. Maecenas iaculis leo a ligula euismod condimentum. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. </p>
-	<p>&not; <a href="#">read more</a></p>
-</div>
-<div class="col last">
-	<h3><a href="#">Quisque consectetur odio</a></h3>
-	<p>Quisque consectetur odio ut sem semper commodo. Maecenas iaculis leo a ligula euismod condimentum. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
-	<p>&not; <a href="#">read more</a></p>
-</div>
+
 <footer>
 	<div id="footer">
 		<p>Pagina generata in {elapsed_time} secondi.
