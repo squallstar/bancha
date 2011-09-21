fb0b6c100a01f31044bbc142f65eb1dd68952f0a
Nicholas Valbusa <info@squallstar.it>
2011-09-21 10:23:44 +0200

    bug fix

--- application/models/bancha/model_records.php
+++ application/models/bancha/model_records.php
@@ -566,7 +566,7 @@ Class Model_records extends CI_Model {
 	          	if ($this->db->insert($this->table_stage, $data))
 	          	{
 		            $done = $this->db->insert_id();
-	            	$this->events->log('insert', $done, $data['title'], $data['id_type']);
+	            	$this->events->log('insert', $done, $data[$tipo['edit_link']], $data['id_type']);
 	          	} else {
 	          		show_error('Impossibile aggiungere il record di tipo ['.$data['id_type'].'].', 500, 'Inserimento record');
 	          	}
