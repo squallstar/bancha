e6c7df7a89dbad07e0b7d3422ba758a3f53f04ee
Nicholas <info@squallstar.it>
2011-09-26 22:00:21 +0200

    issue #29 - groups are now live

--- application/views/admin/users/groups/list.php
+++ application/views/admin/users/groups/list.php
@@ -34,7 +34,6 @@ $this->load->helper('text');
 				<tr>
 					<th>ID</th>
 					<th><?php echo _('Group name'); ?></th>
-					<th><?php echo _('Permissions'); ?></th>
 					<td>&nbsp;</td>
 				</tr>
 			</thead>
@@ -47,9 +46,8 @@ $this->load->helper('text');
 			//Campi ricorrenti
 			echo '<td>'.$group->id_group.'</td>'
 				.'<td><a href="'.admin_url('users/groups/edit/'.$group->id_group).'">'.$group->group_name.'</a></td>';
-			echo '<td></td>';
 			echo '<td class="delete">'
-					.'<a href="'.admin_url('users/group_delete/'.$group->id_group).'" onclick="return confirm(\''._('Do you want to delete this group?').'\');">'._('Delete').'</a>'
+					.'<a href="'.admin_url('users/group_delete/'.$group->id_group).'" onclick="return confirm(\''._('Do you want to delete this group?').'\');">'._('Delete group').'</a>'
 				.'</td>';
 		echo '</tr>';
 	}
