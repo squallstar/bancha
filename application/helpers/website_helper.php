48f8ee77c08f4c52546884d49f394167378ad2fa
Nicholas Valbusa <info@squallstar.it>
2011-09-27 17:02:21 +0200

    new helper: preset_url

--- application/helpers/website_helper.php
+++ application/helpers/website_helper.php
@@ -18,7 +18,8 @@
  * @param string $title
  * @param bool $kill
  */
-function debug($obj, $title='', $kill = FALSE) {
+function debug($obj, $title='*DEBUG*', $kill = FALSE)
+{
 	echo "<pre>-------------------\r\n";
 	if ($title != '') echo strtoupper($title)."\r\n";
 	if (is_string($obj)) $obj = htmlentities($obj);
@@ -47,7 +48,8 @@ function getter($url)
  * Returns the url of the administration
  * @param string $str path to append
  */
-function admin_url($str='') {
+function admin_url($str='')
+{
 	return site_url('admin/'.$str);
 }
 
@@ -55,14 +57,34 @@ function admin_url($str='') {
  * Returns the path of the current theme
  * @param string $str path to append
  */
-function theme_url($str = '') {
+function theme_url($str = '')
+{
 	return THEME_PUB_PATH . $str;
 }
 
 /**
  * Returns the public url of an attachment
- * @param string $str path relativa
+ * @param string $str relative path
  */
-function attach_url($str='') {
-	return site_url(get_instance()->config->item('attach_out_folder') . str_replace('\\', '/', $str));
+function attach_url($str='')
+{
+	return site_url(config_item('attach_out_folder') . str_replace('\\', '/', $str));
+}
+
+/**
+ * Returns the path of an image preset, given the path and the preset name to apply
+ * @param $path image path
+ * @param $preset preset name
+ * @param $append_siteurl whether to prepend or not the website url
+ */
+function preset_url($path, $preset, $append_siteurl = TRUE) {
+	if ($path && $preset)
+	{
+		//Prototype: attach/cache/type/field/id/preset/name.ext
+		$tmp = explode(array('/', '\\'), trim($obj, '/'));
+		$i = count($tmp)-1;
+		$path = config_item('attach_out_folder') . '/cache/' . $tmp[$-3] . '/' . $tmp[$-2] . '/' . $tmp[$i-1] . '/' . $preset . '/' . $tmp[$i];
+		return $append_siteurl ? site_url($path) : $path;
+	}
+	return '';
 }
