20814245dc22bb7a0f79068f446324b4209aba44
Nicholas <info@squallstar.it>
2011-10-01 15:23:17 +0200

    item name

--- application/views/admin/content/record_edit.php
+++ application/views/admin/content/record_edit.php
@@ -22,7 +22,7 @@ $CI = & get_instance();
 		<div class="bheadl"></div>
 		<div class="bheadr"></div>
 
-		<h2><?php echo (!$record->id ? _('New content') : _('Edit content') ) . ': ' . $tipo['description']; ?></h2>
+		<h2><?php echo (!$record->id ? _($tipo['label_new']) : _('Edit content') ) . ': ' . $tipo['description']; ?></h2>
 
 		<ul>
 			<li><img class="middle" src="<?php echo site_url(THEMESPATH.'admin/widgets/icns/arrow_left.png'); ?>" /> <a href="<?php echo admin_url($_section.'/type/'.$tipo['id'])?>"><?php echo _('Back to list'); ?></a></li>
@@ -60,7 +60,7 @@ $p_end = '</p>';
 
 $breadcrumbs_render = '<p class="breadcrumb"><a href="<?php echo admin_url($_section); ?>">'.($_section == 'contents' ? _('Contents') : _('Pages')).'</a> '
 						 . '&raquo; <a href="'.admin_url($_section.'/type/'.$tipo['id']).'">'.$tipo['description'].'</a> &raquo; '
-						 . (!$record->id ? _('New content') : (_('Edit content') . ' &raquo; <strong>' . $record->get($tipo['edit_link']) . '</strong>')) . '</p>';
+						 . (!$record->id ? _($tipo['label_new']) : (_('Edit content') . ' &raquo; <strong>' . $record->get($tipo['edit_link']) . '</strong>')) . '</p>';
 
 foreach ($tipo['fieldsets'] as $fieldset)
 {
--- application/views/admin/content/record_list.php
+++ application/views/admin/content/record_list.php
@@ -13,7 +13,7 @@ $fields = array_keys($tipo['fields']);
 		<h2><?php echo ($tipo['tree']?_('Pages'):_('Contents')).': '.$tipo['description']; ?></h2>
 
 		<ul>
-			<li><img class="middle" src="<?php echo site_url(THEMESPATH.'admin/widgets/icns/plus.png'); ?>" /> <a href="<?php echo admin_url($_section.'/edit_record/'.$tipo['name']); ?>"><?php echo $_section == 'contents' ? _('Insert new content') : _('Insert new page');?></a></li>
+			<li><img class="middle" src="<?php echo site_url(THEMESPATH.'admin/widgets/icns/plus.png'); ?>" /> <a href="<?php echo admin_url($_section.'/edit_record/'.$tipo['name']); ?>"><?php echo _($tipo['label_new']);?></a></li>
 			<?php if ($tipo['has_categories']) { ?><li><img class="middle" src="<?php echo site_url(THEMESPATH.'admin/widgets/icns/bookmark.png'); ?>" /> <a href="<?php echo admin_url($_section.'/type_categories/'.$tipo['name']); ?>"><?php echo _('Manage categories'); ?></a></li><?php } ?>
 		</ul>
 	</div>
