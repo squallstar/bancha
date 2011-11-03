5927249fe8b3cc019532527024e5a47520143842
Nicholas Valbusa <siogiv@MacBook-Pro-di-Sistemi-Informativi-GIV.local>
2011-11-03 15:56:33 +0100

    relation display view

--- application/views/admin/relations/single.php
+++ application/views/admin/relations/single.php
@@ -11,3 +11,25 @@
  * @link		http://squallstar.it
  *
  */
+
+if ($objects instanceof Record)
+{
+	$objects = array($objects);
+}
+if (!count($objects)) return;
+
+$tipo = $this->content->type($objects[0]->_tipo);
+
+foreach ($objects as $relation) {
+	$url = admin_url(
+			($tipo['tree'] ? 'pages' : 'contents') . '/edit_record/'
+			. $tipo['name'] . '/'.$relation->id
+		);
+?>
+<div class="item">
+	<a href="<?php echo $url; ?>"><?php echo $relation->get($tipo['edit_link']); ?></a>
+</div>
+ 
+ 
+<?php	
+}
