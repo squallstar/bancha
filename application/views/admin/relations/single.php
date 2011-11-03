d0ed473898790294ef11b72a8521b66965149204
Nicholas Valbusa <siogiv@MacBook-Pro-di-Sistemi-Informativi-GIV.local>
2011-11-03 15:15:25 +0100

    new view that helps the relations to be rendered

---
+++ application/views/admin/relations/single.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Single render
+ *
+ * Relations package
+ *
+ * @package		Bancha
+ * @author		Nicholas Valbusa - info@squallstar.it - @squallstar
+ * @copyright	Copyright (c) 2011, Squallstar
+ * @license		GNU/GPL (General Public License)
+ * @link		http://squallstar.it
+ *
+ */
