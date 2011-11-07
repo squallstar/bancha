3f74a58caec87d9bc2323c774da9a264fdf2ea68
Nicholas Valbusa <info@squallstar.it>
2011-11-07 12:02:32 +0100

    project cleanup

--- core/logs/log-2011-10-11.php
+++
@@ -1,7 +0,0 @@
-<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
-
-ERROR - 2011-10-11 23:05:33 --> Severity: Notice  --> unserialize() [<a href='function.unserialize'>function.unserialize</a>]: Error at offset 0 of 352 bytes /Web/bancha/application/libraries/codeigniter/libraries/Session.php 723
-ERROR - 2011-10-11 23:12:12 --> Severity: Warning  --> Cannot modify header information - headers already sent by (output started at /Web/bancha/application/helpers/website_helper.php:23) /Web/bancha/application/libraries/codeigniter/core/Output.php 337
-ERROR - 2011-10-11 23:12:14 --> Severity: Warning  --> Cannot modify header information - headers already sent by (output started at /Web/bancha/application/helpers/website_helper.php:23) /Web/bancha/application/libraries/codeigniter/core/Output.php 337
-ERROR - 2011-10-11 23:12:15 --> Severity: Warning  --> Cannot modify header information - headers already sent by (output started at /Web/bancha/application/helpers/website_helper.php:23) /Web/bancha/application/libraries/codeigniter/core/Output.php 337
-ERROR - 2011-10-11 23:12:20 --> Severity: Warning  --> Cannot modify header information - headers already sent by (output started at /Web/bancha/application/helpers/website_helper.php:23) /Web/bancha/application/libraries/codeigniter/core/Output.php 337
--- core/logs/log-2011-10-12.php
+++
@@ -1,7 +0,0 @@
-<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
-
-ERROR - 2011-10-12 01:21:20 --> Severity: Warning  --> Cannot modify header information - headers already sent by (output started at /Web/bancha/application/controllers/admin/ajax.php:67) /Web/bancha/application/libraries/codeigniter/core/Output.php 337
-ERROR - 2011-10-12 01:22:58 --> Severity: Warning  --> Cannot modify header information - headers already sent by (output started at /Web/bancha/application/controllers/admin/ajax.php:67) /Web/bancha/application/libraries/codeigniter/core/Output.php 337
-ERROR - 2011-10-12 01:25:52 --> Severity: Warning  --> Cannot modify header information - headers already sent by (output started at /Web/bancha/application/controllers/admin/ajax.php:67) /Web/bancha/application/libraries/codeigniter/core/Output.php 337
-ERROR - 2011-10-12 01:26:16 --> Severity: Warning  --> Cannot modify header information - headers already sent by (output started at /Web/bancha/application/controllers/admin/ajax.php:67) /Web/bancha/application/libraries/codeigniter/core/Output.php 337
-ERROR - 2011-10-12 01:28:38 --> Severity: Notice  --> A non well formed numeric value encountered /Web/bancha/application/views/admin/content/record_edit.php 195
