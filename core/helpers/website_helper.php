184dfdc5452a22aa338e2c885b4fd390b1761e62
Nicholas <info@squallstar.it>
2011-11-22 22:47:36 +0100

    new helper function: semantic_url()

--- core/helpers/website_helper.php
+++ core/helpers/website_helper.php
@@ -2,7 +2,9 @@
 /**
  * Website Helper
  *
- * Some utilities for the website (back and front end)
+ * Some utilities for the website
+ * ------------------------------
+ * Please do not change the functions below. Instead, feel free to copy and rename them.
  *
  * @package		Bancha
  * @author		Nicholas Valbusa - info@squallstar.it - @squallstar
@@ -81,7 +83,8 @@ function attach_url($str='')
  * @param $preset preset name
  * @param $append_siteurl whether to prepend or not the website url
  */
-function preset_url($path, $preset, $append_siteurl = TRUE) {
+function preset_url($path, $preset, $append_siteurl = TRUE)
+{
 	if ($path && $preset)
 	{
 		//Prototype: attach/cache/type/field/id/preset/name.ext
@@ -91,4 +94,51 @@ function preset_url($path, $preset, $append_siteurl = TRUE) {
 		return $append_siteurl ? site_url($path, FALSE) : $path;
 	}
 	return '';
+}
+
+/**
+ * This function tries to generate the detail following url, based on the current page and the given uri
+ * @param Record|string $object A record object, or just the URI to append
+ */
+function semantic_url($object = '')
+{
+	$CI = & get_instance();
+	$view = & $CI->view;
+	$record = $view->get('record');
+	$page = $view->get('page');
+
+	$current_uri = $CI->uri->uri_string;
+
+	if ($object instanceof Record)
+	{
+		$uri = $object->get('uri');
+	} else {
+		$uri = (string)$object;
+	}
+
+	if ($record)
+	{
+		if ($page->get('action') == 'single')
+		{
+			//We are in the detail page of a record. Let's replace the current detail URI with the new one
+			$parent_uri = rtrim($current_uri, $record->get('uri'));
+			return site_url($parent_uri . $uri);
+		}
+	} else {
+		//We are in a page
+		if ($page->get('action') == 'list')
+		{
+			//If the page is listing some records, we just add the new URI to the current one
+			return site_url(rtrim($current_uri, '/') . '/' . $uri);
+		}
+	}
+
+	//If we are here, is a normal page. We have to try to find another page that is listing this record
+	if ($page && $object instanceof Record)
+	{
+		return site_url($CI->pages->get_semantic_url($object->_tipo) . '/' . $uri);
+	} else {
+		//No more things to try on
+		return '#';
+	}
 }
