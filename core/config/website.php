ff5337de27ce5645b878b5489ffb334dbde95e1a
Nicholas <info@squallstar.it>
2011-12-06 23:25:33 +0100

    sandbox description change

--- core/config/website.php
+++ core/config/website.php
@@ -81,7 +81,7 @@ $sep = DIRECTORY_SEPARATOR;
  * To activate a theme, go to the settings section under "Manage".
  */
 $config['installed_themes'] = array(
-	'sandbox' => 'The default sandbox'
+	'sandbox' => 'Sandbox'
 );
 
 /*
