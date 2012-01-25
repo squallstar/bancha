35de0dfc8024cd42383db8944b1c2da84a889b6f
Nicholas <info@squallstar.it>
2012-01-25 17:21:31 +0100

    version change

--- core/config/website.php
+++ core/config/website.php
@@ -20,7 +20,7 @@ if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 /*
  * Bancha primary version
  */
-define('BANCHA_VERSION', '1.0.7');
+define('BANCHA_VERSION', '1.0.8');
 
 
 /* WEBSITE LANGUAGES
