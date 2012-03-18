9cd4ade6cb7798b090431274221b3dd17367b433
unknown <Matt@Matt-PC.(none)>
2012-03-18 16:10:33 +0100

    html5 boilerplate

--- themes/sandbox/views/layout.php
+++ themes/sandbox/views/layout.php
@@ -15,8 +15,14 @@
  *
  */
 
-?><!doctype html>
-<html class="no-js" lang="<?php echo language(); ?>">
+?>
+<!doctype html>
+<!-- paulirish.com/2008/conditional-stylesheets-vs-css-hacks-answer-neither/ -->
+<!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="<?php echo language(); ?>"> <![endif]-->
+<!--[if IE 7]>    <html class="no-js lt-ie9 lt-ie8" lang="<?php echo language(); ?>"> <![endif]-->
+<!--[if IE 8]>    <html class="no-js lt-ie9" lang="<?php echo language(); ?>"> <![endif]-->
+<!-- Consider adding a manifest.appcache: h5bp.com/d/Offline -->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="<?php echo language(); ?>"> <!--<![endif]-->
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
