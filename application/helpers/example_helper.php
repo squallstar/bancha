a2f401ef2c3fe010b153e2716c8bbf32ba5bd892
Nicholas Valbusa <info@squallstar.it>
2012-04-23 21:11:19 +0200

    example helper added

---
+++ application/helpers/example_helper.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Example helper
+ *
+ * This script is an example of how to implement and use an helper
+ *
+ * How to load an inside the MVC:
+ * $this->load->helper('helper_name');
+ *
+ * And on a theme:
+ * load_helper('helper_name');
+ *
+ * @package		Bancha
+ * @author		Nicholas Valbusa - info@squallstar.it - @squallstar
+ * @copyright	Copyright (c) 2011-2012, Squallstar
+ * @license		GNU/GPL (General Public License)
+ * @link		http://squallstar.it
+ *
+ */
+
+if (!function_exists('example'))
+{
+	function example()
+	{
+		echo 'hello world';
+	}
+}
